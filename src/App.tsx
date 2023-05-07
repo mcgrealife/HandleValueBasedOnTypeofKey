110f4f56fd50c1ce0884d722f637190f82474571
Michael McGreal <mcgrealife@gmail.com>
2023-05-07 16:03:20 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+type SomeThing = {
+  thing1: string;
+  thing2: string[];
+};
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <HandleValueBasedOnTypeofKey
+        key="thing1"
+        value="this has \n line breaks, but key thing1 is typeof string, so keep as string"
+      />
+      <HandleValueBasedOnTypeofKey
+        key="thing2"
+        value="this also has \n line breaks, but key thing2 is typeof string[], so value.split('\n')"
+      />
+    </>
+  );
+}
+
+async function HandleValueBasedOnTypeofKey({
+  value,
+  key,
+}: {
+  value: string;
+  key: keyof SomeThing;
+}) {
+  return Array.isArray(typeof SomeThing[key]) ? (
+    value.split("\n").map((line) => <p>{line}</p>)
+  ) : (
+    <p>{value}</p>
+  );
 }
 
 export default App;
